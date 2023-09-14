25cc9a9004a29a58d26aad964a9778521da89a77
fatemanazari <fatemanazari78@gmail.com>
2023-09-15 01:01:46 +0330

    fix errors

--- src/components/profile/rockets/Rockets.test.jsx
+++ src/components/profile/rockets/Rockets.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import React from 'react';
 import { render, waitFor, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
